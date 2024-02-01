c478c54bdad67b9844c12a74166cd50c384d292c
oscaroguledo <122152636+oscaroguledo@users.noreply.github.com>
2024-02-01 18:21:15 +0000

    Committing local changes before merging

--- f-oahse-app/src/App.js
+++ f-oahse-app/src/App.js
@@ -7,7 +7,7 @@ import './App.css';
 // import 'swiper/css/effect-fade';
 // import 'swiper/css/navigation';
 // import 'swiper/css/pagination';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter,HashRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home';
 import Find from './pages/Find';
 import About from './pages/About';
@@ -16,12 +16,16 @@ import About from './pages/About';
 
 
 function App() {
+  const ROOT_URL="http://localhost:3000/"; //"https://oahse.github.io/O-sell/"
+  const findurl = ROOT_URL+"/find/";
+  const abouturl = ROOT_URL+"/about/";
+  console.log(findurl);
   return (
     <BrowserRouter>
         <Routes>
-            <Route path='/' element={<Home/>} exact ></Route>
-            <Route path='/find/' element={<Find/>} ></Route>
-            <Route path='/about/' element={<About/>} ></Route>
+            <Route path="/" element={<Home/>} exact ></Route>
+            <Route path="/find/" element={<Find/>} ></Route>
+            <Route path="/about/" element={<About/>} ></Route>
         </Routes>
     </BrowserRouter>
   );
