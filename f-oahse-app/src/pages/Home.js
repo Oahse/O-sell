2ae053c2363d4e110636ff55fb177b87159a928b
Kosy <okekekosy12@gmail.com>
2024-03-10 00:30:55 +0100

    update on homePage responsiveness

--- f-oahse-app/src/pages/Home.js
+++ f-oahse-app/src/pages/Home.js
@@ -8,6 +8,8 @@ import SideNavBar from '../components/MobileSideBar';
 import Btn from '../components/Button';
 import IconButton from '../components/Iconbutton';
 import Search from '../components/Search';
+import {Button, Card} from 'react-bootstrap'
+import metalogo from '../assets/Meta-Logo.png'
 
 
 function Home (props){
@@ -75,34 +77,193 @@ function Home (props){
             <Header iconColor={iconColor} navbarBg={navbarBg} linkstyles={linkstyles} />
             <SideNavBar iconColor={iconColor}/>
 
-            <div  className='supportPage' style={{ position: 'relative', display: 'flex', alignItems: 'center', justifyContent: 'flex-start',}}>
+            <div className='showcase-pic' >
+            <Container >
+                <Row className='showcase' style={{ alignItems: 'center', justifyContent: 'center'}}>
+                    
+                    <Col className='' lg={8} md={12} sm={12}>
+                                    <h1>Your Engineering Services <br/>
+                                        Made Easier,
+                                        Made Better
+                                    </h1>
+                                    <p>
+                                        At the Comfort of your space,  get in-touch with  <br/>
+                                        professional engineers in seconds, <br/>
+                                        Get easier access to tradespersons around you in minutes  <br/>
+                                        for your on-time delivery of services; <br/>
+                                        and skip the traffic to get your technical supplies delivered  <br/>
+                                        to you at your door-step <br/>
+                                    </p>
+                        </Col>
+                    <Col className=''>
+                        <IconButton icon={faGooglePlay} to="#" text="Google Play" className='m-0' />
+                        <IconButton icon={faAppStore} to="#" text="App Store" className='m-0' />
+                    </Col>
+                </Row>
+            </Container>
+            </div>
+           
+           
+
+            {/* <div  className='supportPage' style={{ position: 'relative', display: 'flex', alignItems: 'center', justifyContent: 'flex-start',}}>
                 <img src={CIPAC_cover_photo} alt='support' width='100%' style={{filter: "brightness(70%)"}}/>
 
                 <Row style={{position: 'absolute', color: 'white', }}>
                     <Container>
-                    <Col className='' >
-                <h1>Your Engineering Services <br/>
-                    Made Easier,
-                    Made Better</h1>
-                    <small>
-                          At the Comfort of your space,  get in-touch with  <br/>
-                          professional engineers in seconds, <br/>
-                          Get easier access to tradespersons around you in minutes  <br/>
-                          for your on-time delivery of services; <br/>
-                          and skip the traffic to get your technical supplies delivered  <br/>
-                          to you at your door-step <br/>
-                        </small>
-            </Col>
+                        <Col className=' mb-3' >
+                            <h1>Your Engineering Services <br/>
+                                Made Easier,
+                                Made Better
+                            </h1>
+                            <small>
+                                At the Comfort of your space,  get in-touch with  <br/>
+                                professional engineers in seconds, <br/>
+                                Get easier access to tradespersons around you in minutes  <br/>
+                                for your on-time delivery of services; <br/>
+                                and skip the traffic to get your technical supplies delivered  <br/>
+                                to you at your door-step <br/>
+                            </small>
+                        </Col>
                 
-            <Col >
-                <IconButton icon={faGooglePlay} to="#" text="Google Play" className='m-0' />
-                <IconButton icon={faAppStore} to="#" text="App Store" className='m-0' />
-            </Col>
+                        <Col >
+                            <IconButton icon={faGooglePlay} to="#" text="Google Play" className='m-0 mb-3' />
+                            <IconButton icon={faAppStore} to="#" text="App Store" className='m-0' />
+                        </Col>
                     </Container>
-       
                 </Row>
 
-        </div> 
+            </div>  */}
+
+            <Container>
+
+                <Row className='py-3'>
+                    <Row className='my-3'>
+                        <Col className='text-center'><h1>Our Services</h1></Col>
+                        
+                    </Row>
+
+                    <Row>
+                        <Col className='mb-3'>
+                            <Card style={{ width: '18rem', height: '320px' }}>
+                                <Card.Body>
+                                    <Card.Title>Engineering Services</Card.Title>
+                                    <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                                    <Card.Text>
+                                        Work with top 
+                                        professional and
+                                        experienced engineers
+                                        in handling your engineering
+                                        projects ranging from 
+                                        consultation and design
+                                        to supervision to 
+                                        completion with standard
+                                        quality.
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+
+                        <Col>
+                            <Card style={{ width: '18rem', height: '320px' }}>
+                                <Card.Body>
+                                    <Card.Title>Supply/Delivery</Card.Title>
+                                    <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                                    <Card.Text>
+                                        Order your materials from
+                                        our online store and 
+                                        experience and on-time
+                                        supply/ delivery of your 
+                                        quality materials. 
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+
+                        <Col>
+                            <Card style={{ width: '18rem', height: '320px'}}>
+                                <Card.Body>
+                                    <Card.Title>Maintainence Services</Card.Title>
+                                    <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                                    <Card.Text>
+                                        Find and Request for 
+                                        services from highly skilled 
+                                        and experienced 
+                                        tradespersons and 
+                                        technicians around you. 
+                                        Get them at the comfort 
+                                        of your home and experience 
+                                        service at its best.
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+
+
+                    </Row>
+
+                </Row>
+
+                <Row className='py-3'>
+                    <Col className='text-center py-3' lg={12} md={12} sm={12}>
+                        <h1>Our Clients</h1>
+                    </Col>
+
+                    <Col>
+                        <img src={metalogo} alt='meta logo'/>
+                    </Col>
+
+                    <Col>
+                        <img src={metalogo} alt='meta logo'/>
+                    </Col>
+
+                    <Col>
+                        <img src={metalogo} alt='meta logo'/>
+                    </Col>
+
+
+                </Row>
+
+                <Row>
+                    <Col>
+                        <h1>Earn with us </h1>
+                    </Col>
+
+                    <Col>
+                        
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col>
+                        <h2>What do you need? Let's supply it</h2>
+                    </Col>
+
+                    <Col>
+
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col>
+                        <h2>News and Updates</h2>
+                    </Col>
+
+                    <Col></Col>
+                </Row>
+
+                <Row className='text-center py-3 mb-3'>
+                    <Col lg={12} md={12} sm={12}>
+                        <h2>It is easier on our App</h2>
+                    </Col>
+
+                    <Col className='py-3'>
+                        <a href='/'><Button variant='outline-warning' size='lg' className='rounded-5'>Download the App </Button></a>
+                    </Col>
+                </Row>
+
+            </Container>
+
+           
             
             
             <Footer className='footer'/>
