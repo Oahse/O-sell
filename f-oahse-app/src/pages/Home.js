f5321f30a7c93a1a8235443030ea954e7e40585f
Kosy <okekekosy12@gmail.com>
2024-03-11 00:16:01 +0100

    Refurbish HomePage

--- f-oahse-app/src/pages/Home.js
+++ f-oahse-app/src/pages/Home.js
@@ -9,7 +9,11 @@ import Btn from '../components/Button';
 import IconButton from '../components/Iconbutton';
 import Search from '../components/Search';
 import {Button, Card} from 'react-bootstrap'
-import metalogo from '../assets/Meta-Logo.png'
+import metalogo from '../assets/Meta-Logo2.png'
+import abblogo from '../assets/ABB-LOGO.png'
+import ericsson from '../assets/ericsson-logo.jpg'
+import chevron from '../assets/chevron-logo.png' 
+import AppScreen from '../assets/localhost_3000_O-sell(iPhone 6_7_8 Plus).png'
 
 
 function Home (props){
@@ -142,9 +146,9 @@ function Home (props){
                         
                     </Row>
 
-                    <Row>
-                        <Col className='mb-3'>
-                            <Card style={{ width: '18rem', height: '320px' }}>
+                    <Row >
+                        <Col className='mb-3 d-flex justify-content-center align-items-center'>
+                            <Card style={{ width: '18rem', height: '320px' }} >
                                 <Card.Body>
                                     <Card.Title>Engineering Services</Card.Title>
                                     <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
@@ -163,7 +167,7 @@ function Home (props){
                             </Card>
                         </Col>
 
-                        <Col>
+                        <Col className='mb-3 d-flex justify-content-center align-items-center' >
                             <Card style={{ width: '18rem', height: '320px' }}>
                                 <Card.Body>
                                     <Card.Title>Supply/Delivery</Card.Title>
@@ -179,7 +183,7 @@ function Home (props){
                             </Card>
                         </Col>
 
-                        <Col>
+                        <Col className='mb-3 d-flex justify-content-center align-items-center'>
                             <Card style={{ width: '18rem', height: '320px'}}>
                                 <Card.Body>
                                     <Card.Title>Maintainence Services</Card.Title>
@@ -203,62 +207,164 @@ function Home (props){
 
                 </Row>
 
-                <Row className='py-3'>
-                    <Col className='text-center py-3' lg={12} md={12} sm={12}>
+                <Row className='py-3 text-center'>
+                    <Col className='text-center py-3 mb-3' lg={12} md={12} sm={12}>
                         <h1>Our Clients</h1>
                     </Col>
 
-                    <Col>
+
+                    <Col className='mb-3'>
                         <img src={metalogo} alt='meta logo'/>
                     </Col>
 
-                    <Col>
-                        <img src={metalogo} alt='meta logo'/>
+                    <Col className='mb-3'>
+                        <img src={abblogo} alt='meta logo' />
                     </Col>
 
-                    <Col>
-                        <img src={metalogo} alt='meta logo'/>
+                    <Col className='mb-3'>
+                        <img src={ericsson} alt='meta logo' />
                     </Col>
 
+                    <Col className='mb-3'>
+                        <img src={chevron} alt='meta logo' />
+                    </Col>
+
+                   
 
                 </Row>
 
-                <Row>
-                    <Col>
+                <Row className='mb-5'>
+                    <Col className='py-3 text-center' lg={12} md={12} sm={12}>
                         <h1>Earn with us </h1>
                     </Col>
 
-                    <Col>
-                        
+                    <Col className='text-center' lg={6}  sm={12}>
+                        <div className='p-3'>
+                            <img src='https://images.pexels.com/photos/8961146/pexels-photo-8961146.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='engineer' width='100%'/> 
+                        </div>
+                       
+                    </Col>
+
+                    <Col className='py-3'> 
+                        <h2 className='mb-3'>Become an Engineering Consultant</h2>
+                        <p>
+                            Work with experienced engineers in handling your projects 
+                            ranging from consultation and design to supervision to completion with standard quality.
+                        </p>
+
+                        <p>
+                        Drive for as long and as often as you like. Weekdays; weekends; evenings — fit driving around your lifestyle.
+                        </p>
+
+                        <p>
+                            Order your materials from our online store and experience 
+                            and on-time supply/ delivery of your quality materials.
+                        </p>
+
+                        <Button className='rounded-5'>Learn More</Button>
                     </Col>
                 </Row>
 
-                <Row>
-                    <Col>
+                <Row style={{backgroundColor: 'grey', color: 'whitesmoke'}} className='rounded-4 text-center mx-3'>
+                    <Col className='py-3 text-center' lg={12} md={12} sm={12}>
                         <h2>What do you need? Let's supply it</h2>
                     </Col>
 
                     <Col>
-
+                        <div>
+                            <img src='https://images.pexels.com/photos/257736/pexels-photo-257736.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='product' width='100%'/>
+                           
+                        </div>
+                        <p>Cables</p>
                     </Col>
-                </Row>
 
-                <Row>
                     <Col>
-                        <h2>News and Updates</h2>
+                        <div>
+                            <img src='https://images.pexels.com/photos/159108/light-lamp-electricity-power-159108.jpeg' alt='product' width='100%'/>
+                            <p>Lightings</p>
+                        </div>
                     </Col>
 
-                    <Col></Col>
+                    <Col>
+                        <div>
+                            <img src='https://images.pexels.com/photos/60049/torx-bits-metal-iron-60049.jpeg?auto=compress&cs=tinysrgb&w=600' alt='product' width='100%'/>
+                            <p>Tools and Fittings</p>
+                        </div>
+                    </Col>
+
+                    <Col>
+                        <div>
+                            <img src='https://images.pexels.com/photos/433308/pexels-photo-433308.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='product' width='100%'/>
+                            <p>Power and Back-up</p>
+                        </div>
+                    </Col>
+
+                    <Col>
+                        <div>
+                            <img src='https://images.pexels.com/photos/2842460/pexels-photo-2842460.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='product' width='100%'/>
+                            <p>Engineering Accessories</p>
+                        </div>
+                    </Col>
                 </Row>
 
-                <Row className='text-center py-3 mb-3'>
-                    <Col lg={12} md={12} sm={12}>
-                        <h2>It is easier on our App</h2>
-                    </Col>
+                <Row className='py-3'>
+                    <Col className='py-3' lg={12} md={12} sm={12}>
+                        <Row>
+                            <Col>
+                                <h2>News and Updates</h2>
+                            </Col>
 
+                            <Col className='text-end'>
+                                <h3>View all </h3>
+                            </Col>
+                        </Row>
+                       
+                    </Col>
+                        
+                    <Col>
+                         <div>
+                            <img src={CIPAC_cover_photo} alt='news' width='100%'/>
+                        </div>
+                        <h3>SPAWN Engineering Fest 2024</h3>
+                        <p>
+                            Order your materials from our online store 
+                            and experience and on-time supply/ delivery of your quality materials.
+                        </p>
+                    </Col>
+                       
+                    <Col>
+                         <div>
+                            <img src={CIPAC_cover_photo} alt='news' width='100%'/>
+                        </div>
+                        <h3>SPAWN Engineering Fest 2024</h3>
+                        <p>
+                            Order your materials from our online store 
+                            and experience and on-time supply/ delivery of your quality materials.
+                        </p>
+                    </Col>
+                       
+                    <Col>
+                        <div>
+                            <img src={CIPAC_cover_photo} alt='news' width='100%'/>
+                        </div>
+                        <h3>SPAWN Engineering Fest 2024</h3>
+                        <p>
+                            Order your materials from our online store 
+                            and experience and on-time supply/ delivery of your quality materials.
+                        </p>
+                    </Col>
+                </Row>
+
+                <Row className='text-center rounded-5 py-3 mb-3 mx-3'style={{backgroundColor: 'black', color: 'white'}}>
                     <Col className='py-3'>
+                        <img src={AppScreen} alt='App' width='200px'/>
+                    </Col>
+                    <Col className='py-5'>
+                        <h2>It is easier on our App</h2>
+                        <p>Available for Android and iOS devices</p>
                         <a href='/'><Button variant='outline-warning' size='lg' className='rounded-5'>Download the App </Button></a>
                     </Col>
+
                 </Row>
 
             </Container>
